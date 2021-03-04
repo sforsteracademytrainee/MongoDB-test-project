b0077abb77ba846dcd26dcd1d2485766264b6711
Simon Forster <sforster@academytrainee.com>
2021-03-04 11:16:30 +0000

    sawefr

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,12 +2,24 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import org.mongodb.scala._
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def test = Action {
+    val uri: String = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.nveej.mongodb.net/sample_analytics?retryWrites=true&w=majority"
+    System.setProperty("org.mongodb.async.type", "netty")
+    val client: MongoClient = MongoClient(uri)
+    val db: MongoDatabase = client.getDatabase("sample_analytics")
+    val coll: MongoCollection[Document] = db.getCollection("customers")
+    val results = coll.find()
+    println(results.first().document("_id"))
+    Ok(views.html.index("aaaa"))
   }
 
 }
